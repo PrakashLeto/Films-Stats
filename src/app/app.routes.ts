33482544c42699a8f5e1bf6e68df6dc15616a988
PrakashLeto <prakashz2012@gmail.com>
2025-02-09 23:47:50 +0530

    Delete src directory

--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { StatsComponent } from './stats/stats.component';
-
-export const routes: Routes = [
-    { path: '', component: HomeComponent },
-    { path: 'home', component: HomeComponent },
-    { path: 'stats', component: StatsComponent}
-];
