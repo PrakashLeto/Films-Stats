a988d338ab662b0d369d162c91224197f18b3d57
Prakash Subramaniam <210319@usthealthproof.com>
2025-02-09 22:01:38 +0530

    Initial commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { StatsComponent } from './stats/stats.component';
+
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'home', component: HomeComponent },
+    { path: 'stats', component: StatsComponent}
+];
